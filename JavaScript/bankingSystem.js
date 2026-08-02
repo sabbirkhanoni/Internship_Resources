837212585f1352f2fa391a5dd5b6cd0ec7c88e21
mdsabbirkhanoni <mdsabbirkhanoni@gmail.com>
2026-08-02 12:50:32 +0600

    learn banking system exception handling and rollback

---
+++ JavaScript/bankingSystem.js
@@ -0,0 +1,28 @@
+const accounts = {
+  a: 100,
+  b: 0,
+  c: 20
+};
+
+function getAccount() {
+  let accountName = prompt("Enter an account name");
+  if (!Object.hasOwn(accounts, accountName)) {
+    throw new Error(`No such account: ${accountName}`);
+  }
+  return accountName;
+}
+
+function transfer(from, amount) {
+  if (accounts[from] < amount) return;
+  let progress = 0;
+  try {
+    accounts[from] -= amount;
+    progress = 1;
+    accounts[getAccount()] += amount;
+    progress = 2;
+  } finally {
+    if (progress == 1) {
+      accounts[from] += amount;
+    }
+  }
+}

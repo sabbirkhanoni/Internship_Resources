17ad34a43de89082a4ee10d34b9e6b33829c54c7
mdsabbirkhanoni <mdsabbirkhanoni@gmail.com>
2026-08-02 12:32:46 +0600

    learn bugs and error

---
+++ JavaScript/bugs.js
@@ -0,0 +1,9 @@
+function canYouSpotTheProblem() {
+  "use strict";
+  var counter;
+  for (counter = 0; counter < 10; counter++) {
+    console.log("Happy happy");
+  }
+}
+
+canYouSpotTheProblem();

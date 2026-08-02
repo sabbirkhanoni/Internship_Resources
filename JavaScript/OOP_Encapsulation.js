26fe8f370e69633dfcc04d18649b5b104a6ac24a
mdsabbirkhanoni <mdsabbirkhanoni@gmail.com>
2026-08-02 16:48:30 +0600

    Practice OOP Cocept of Encapsulation

---
+++ JavaScript/OOP_Encapsulation.js
@@ -0,0 +1,17 @@
+class BankAccount {
+  #balance = 0;
+
+  deposit(amount) {
+    this.#balance += amount;
+  }
+
+  getBalance() {
+    return this.#balance;
+  }
+}
+
+const account = new BankAccount();
+
+account.deposit(500);
+
+console.log(account.getBalance());
